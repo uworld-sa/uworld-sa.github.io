24b008dc1c5ac8f1cd5c7880bd70c1e4b4da4e83
uworld.sa <uworld.sa@gmail.com>
2019-03-05 22:27:56 +0200

    new comment

--- homework13 (mvc ToDo) webpack/src/js/hw13.js
+++
@@ -1,5 +0,0 @@
-import ListController from './todo/controller/list';
-
-document.addEventListener('DOMContentLoaded', function documentReady() {
-    new ListController(document.getElementById('buy-list'));
-});
