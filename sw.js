e1d5e486f66d27f7d1fa014e2497693387992890
uworld.sa <uworld.sa@gmail.com>
2019-03-06 18:52:56 +0200

    sw

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener('fetch', function(event) {
                 return response;
             });
         }).catch(function() {
-            //return caches.match('/sw-test/gallery/myLittleVader.jpg');
+            return caches.match('/#404');
         })
     );
 });
